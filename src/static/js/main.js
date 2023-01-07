f5759f209c2cc757310a77da4127805e7f32ad95
John Quinn <john.quinn.cert@gmail.com>
2023-01-07 19:31:12 +0000

    Fixing homepage layout

---
+++ src/static/js/main.js
@@ -0,0 +1,20 @@
+function initYouTubeVideos() {
+    var playerElements = document.getElementsByClassName('youtube-player');
+    for (var n = 0; n < playerElements.length; n++) {
+      var videoId = playerElements[n].dataset.id;
+      var div = document.createElement('div');
+      div.setAttribute('data-id', videoId);
+      var thumbNode = document.createElement('img');
+      thumbNode.src = '//i.ytimg.com/vi/ID/hqdefault.jpg'.replace('ID', videoId);
+      thumbNode.alt = "Click to play video";
+      div.appendChild(thumbNode);
+      var playButton = document.createElement('div');
+      playButton.setAttribute('class', 'play');
+      div.appendChild(playButton);
+      div.onclick = function () {
+        labnolIframe(this);
+      };
+      playerElements[n].appendChild(div);
+    }
+  }
+  document.addEventListener('DOMContentLoaded', initYouTubeVideos);
