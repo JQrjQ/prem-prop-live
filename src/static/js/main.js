387230c54d4c8971df23e6a610ce881201ad3538
John Quinn <john.quinn.cert@gmail.com>
2023-01-08 20:04:32 +0000

    working on homepage

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,25 @@
+ ///////////////////////////////
+ //Javascript to toggle the menu
+ ////////////////////
+ document.getElementById('nav-toggle').onclick = function () {
+  document.getElementById("nav-content").classList.toggle("hidden");
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.zIndex = "500";
+  } else {
+    document.getElementById("navbar").style.top = "-75px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+///////////////
+//Youtube//
+//////////////
 function initYouTubeVideos() {
     var playerElements = document.getElementsByClassName('youtube-player');
     for (var n = 0; n < playerElements.length; n++) {
